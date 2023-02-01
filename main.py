338962575650f8eb3cc9d7f4c3c68aa6236ef95d
Kimtai <kymdennis3@gmail.com>
2023-02-01 08:46:02 +0300

    converted to integer

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ toys = {"robot": "$40.0", "car": "$25", "ironman": "$12"}
 
 sum = list(toys.values())
 
-total = (eval(sum[0][1:]) + eval(sum[1][1:]) + eval(sum[2][1:]))
+total = int((eval(sum[0][1:]) + eval(sum[1][1:]) + eval(sum[2][1:])))
 
 print(total)
